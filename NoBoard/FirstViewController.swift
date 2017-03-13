65f1cead167a8ef1f4d84a29f154f3b7db171d74
Zhao Wang <zhao@platoteam.com>
2017-03-13 15:37:40 -0700

    Initial Commit

---
+++ NoBoard/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NoBoard
+//
+//  Created by Zhao Wang on 3/13/17.
+//  Copyright © 2017 Zhao Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
